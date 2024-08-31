fdb86a235384cdc43577f63db5059f69eb98f552
Joseph Agboeze <josephebube483@gmail.com>
2024-08-31 05:52:17 +0100

    Update Mysql.sql

--- assets/MySQL Query/Mysql.sql
+++ assets/MySQL Query/Mysql.sql
@@ -406,30 +406,5 @@ ORDER BY StockoutDays desc;
 END//
 Delimiter ;
 
--- FEEDBACK LOOPS
--- Feedback Loop Establishment:
--- Feedback Portal: Develop an online platform for stakeholders to easily submit feedback on inventory performance and challenges.
--- Review Meetings: Organize periodic sessions to discuss inventory system performance and gather direct insights.
--- System Monitoring: Use established SQL procedures to track system metrics, with deviations from expectations flagged for review.
--- Refinement Based on Feedback:
--- Feedback Analysis: Regularly compile and scrutinize feedback to identify recurring themes or pressing issues.
--- Action Implementation: Prioritize and act on the feedback to adjust reorder points, safety stock levels, or overall processes.
--- Change Communication: Inform stakeholders about changes, underscoring the value of their feedback and ensuring transparency.
-
--- INSIGHTS
--- Inventory Discrepancies
--- The initial stages of the analysis revealed significant discrepancies in inventory levels, with instances of overstocking. These inconsistencies were contributing to customer capital inefficiencies and dissatisfaction.
--- Sales Trends and External Influences
--- The analysis indicated that sales trends were notably influenced by various external factors. Recognizing these patterns presents an opportunity to forecast demand more accurately.
--- Suboptimal Inventory Levels
--- Through the inventory optimization analysis, it was evident that the existing inventory levels were not optimized for current sales trends. Products were identified that had either close excess inventory.
-
--- RECOMMEDATIONS
--- 1. Implement Dynamic Inventory Management: The company should transition from a static to a dynamic management system, adjusting inventory levels based on real-time sales trends, seasonality, and external factors.
--- 2. Optimize Reorder Points and Safety Stocks: Use the reorder points and safety stocks calculated during the analysis to minimize stockouts and reduce excess inventory. Regularly review these metrics to ensure they align with current market conditions.
--- 3. Enhance Pricing Strategies: Conduct a thorough review of product pricing strategies, especially for products identified as unprofitable. Consider factors such as competitor pricing, market demand, and product acquisition costs.
--- 4. Reduce Overstock: Identify products that are consistently overstocked and take steps to reduce their inventory levels. This could include promotional sales, discounts, or even discontinuing products with low sales performance.
--- 5. Establish a Feedback Loop: Develop a systematic approach to collect and analyze feedback from various stakeholders. Use this feedback for continuous improvement and alignment with business objectives.
--- 6. Regular Monitoring and Adjustments: Adopt a proactive approach to inventory management by regularly monitoring key metrics and making necessary adjustments to inventory levels, order quantities, and safety stocks.
 
     
